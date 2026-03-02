2addaef32e4e7bc1f32ae0a96ea3f72a4ad35c39
Iain Watt <iainskwatt@gmail.com>
2026-03-02 20:57:24 +0000

    added html generator

---
+++ src/gencontent.py
@@ -0,0 +1,29 @@
+import os
+
+from markdown_blocks import markdown_to_html_node
+
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("no title found")
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, 'r') as data:
+        md = data.read()
+    with open(template_path) as data:
+        template = data.read()
+    node = markdown_to_html_node(md)
+    html = node.to_html()
+
+    title = extract_title(md)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
--- src/main.py
+++ src/main.py
@@ -2,9 +2,12 @@ import os
 import shutil
 
 from textnode import TextNode
+from gencontent import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def clear_public_folder():
     print("Deleting public directory...")
@@ -15,7 +18,7 @@ def copy_files_to_public(source, dest):
     print("Populating public directory")
     if not os.path.exists(dest):
         os.mkdir(dest)
-    
+
     for filename in os.listdir(source):
         from_path = os.path.join(source, filename)
         dest_path = os.path.join(dest, filename)
@@ -25,10 +28,17 @@ def copy_files_to_public(source, dest):
         else:
             copy_files_to_public(from_path, dest_path)
 
+def gen_page():
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
 def main():
     clear_public_folder()
     copy_files_to_public(dir_path_static, dir_path_public)
+    gen_page()
 
 if __name__ == "__main__":
     main()
